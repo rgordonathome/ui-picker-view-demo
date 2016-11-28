b9bee1655a097dde2634e27bff32201283845807
rgordonatrsgc-student <student@russellgordon.ca>
2016-11-28 15:12:46 -0500

    Established outlets for the label and the picker view to the view controller.

--- SwiftUIPickerFormattted/ViewController.swift
+++ SwiftUIPickerFormattted/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myPicker: UILabel!
+    @IBOutlet weak var myLabel: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
